db17c692adb965ac9d8a4c857fa1edc28db8304b
Bhargav <177198437+Bhargavxyz738@users.noreply.github.com>
2025-08-26 19:11:21 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 import time
+import inspect
+import os
 
+GRAY    = "\033[90m"
 GREEN   = "\033[32m"
 CYAN    = "\033[36m"
 YELLOW  = "\033[33m"
@@ -12,23 +15,39 @@ RESET   = "\033[0m"
 def timestamp() -> str:
     return time.strftime("%I:%M:%S %p")
 
+def caller_info():
+    frame = inspect.stack()[2]
+    filename = os.path.basename(frame.filename)
+    line = frame.lineno
+    return f"in {filename} at line {line}"
+
 class Debug:
     @staticmethod
     def info(text: str) -> None:
-        print(GREEN + f"[{timestamp()} INFO]" + RESET + " " + WHITE + text + RESET)
+        print(GRAY + f"[{timestamp()} " + GREEN + "INFO" + GRAY + "]" + RESET + " " +
+              GRAY + f"[{caller_info()}]" + RESET + " " +
+              WHITE + text + RESET)
 
     @staticmethod
     def debug(text: str) -> None:
-        print(CYAN + f"[{timestamp()} DEBUG]" + RESET + " " + WHITE + text + RESET)
+        print(GRAY + f"[{timestamp()} " + CYAN + "DEBUG" + GRAY + "]" + RESET + " " +
+              GRAY + f"[{caller_info()}]" + RESET + " " +
+              WHITE + text + RESET)
 
     @staticmethod
     def warn(text: str) -> None:
-        print(YELLOW + f"[{timestamp()} WARNING]" + RESET + " " + WHITE + text + RESET)
+        print(GRAY + f"[{timestamp()} " + YELLOW + "WARNING" + GRAY + "]" + RESET + " " +
+              GRAY + f"[{caller_info()}]" + RESET + " " +
+              WHITE + text + RESET)
 
     @staticmethod
     def err(text: str) -> None:
-        print(RED + f"[{timestamp()} ERROR]" + RESET + " " + WHITE + text + RESET)
+        print(GRAY + f"[{timestamp()} " + RED + "ERROR" + GRAY + "]" + RESET + " " +
+              GRAY + f"[{caller_info()}]" + RESET + " " +
+              WHITE + text + RESET)
 
     @staticmethod
     def succ(text: str) -> None:
-        print(MAGENTA + BRIGHT + f"[{timestamp()} SUCCESS]" + RESET + " " + WHITE + text + RESET)
+        print(GRAY + f"[{timestamp()} " + BRIGHT + MAGENTA + "SUCCESS" + GRAY + "]" + RESET + " " +
+              GRAY + f"[{caller_info()}]" + RESET + " " +
+              WHITE + text + RESET)
